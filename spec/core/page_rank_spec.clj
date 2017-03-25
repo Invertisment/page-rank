a98f2f2ca748015eb8f7e3af30a67267bdf8f862
invertisment <visiskaj.slaptaj@gmail.com>
2017-03-25 23:04:54 +0200

    neighbour weights, merge of weights

--- spec/core/page_rank_spec.clj
+++ spec/core/page_rank_spec.clj
@@ -3,9 +3,65 @@
             [core.page-rank :as a]))
 
 (describe
-  "should get neighbours"
-  (it "should get one neighbour from map"
-      (should=
-        (a/smth)
-        3)))
+  "should get neighbour weight"
+  (letfn [(weight=
+            ([in out]
+             (should=
+               out
+               (a/count-weights in 1)))
+            ([in my-weight out]
+             (should=
+               out
+               (a/count-weights in my-weight))))]
+    (it "nop"
+      (weight= {} []))
+    (it "should find count-weights [count 1 my-weight 1]"
+      (weight=
+        #{"mif.vu.lt"}
+        [["mif.vu.lt" 1]]))
+    (it "should find count-weights [count 2 my-weight 1]"
+      (weight=
+        #{"mif.vu.lt" "google.com"}
+        [["mif.vu.lt" 1/2]
+         ["google.com" 1/2]]))
+    (it "should find count-weights [count 3 my-weight 1]"
+      (weight=
+        #{"mif.vu.lt" "google.com" "onemillionetherpage.com"}
+        [["mif.vu.lt" 1/3]
+         ["google.com" 1/3]
+         ["onemillionetherpage.com" 1/3]]))
+    (it "should find count-weights [count 2 my-weight rand]"
+      (let [factor (rand)
+            div (/ factor 2)]
+        (weight=
+          #{"mif.vu.lt" "google.com"}
+          factor
+          [["mif.vu.lt" div]
+           ["google.com" div]])))))
+
+(describe
+  "should merge neighbours"
+  (letfn [(merge-weights=
+            [in out]
+            (should=
+              out
+              (a/merge-weights in)))]
+    (it "nop"
+      (merge-weights= {} {}))
+    (it "should sum two neighbours"
+      (merge-weights=
+        [["google.com" 1]
+         ["google.com" 2]]
+        {"google.com" 3}))
+    (it "should sum multi"
+      (merge-weights=
+        [["google.com" 2]
+         ["mif.vu.lt" 15]
+         ["onemillionetherpage.com" 1]
+         ["google.com" 1]
+         ["onemillionetherpage.com" 3]
+         ["mif.vu.lt" 25]]
+        {"google.com" 3
+         "mif.vu.lt" 40
+         "onemillionetherpage.com" 4}))))
 
--- src/core/page_rank.clj
+++ src/core/page_rank.clj
@@ -1,7 +1,16 @@
 (ns core.page-rank)
 
-(defn smth []
-  (+
-   1
-   2))
+(defn count-weights [ngbs my-weight]
+  (map
+    (fn [title]
+      [title (/ my-weight (count ngbs))])
+    ngbs))
+
+(defn merge-weights [wgts]
+  (reduce
+    #(merge-with + %1 %2)
+    {}
+    (map
+      #(into {} [%])
+      wgts)))
 
